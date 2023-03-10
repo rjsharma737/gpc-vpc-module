96d1c24d16f43509357762cda40301e022aa983c
rjsharma737 <rjsharma737@gmail.com>
2023-03-10 18:31:41 +0530

    Update apis.tf

--- terraform-network-module/vpc-network/apis.tf
+++ terraform-network-module/vpc-network/apis.tf
@@ -1,6 +1,7 @@
 resource "google_project_service" "enable_apis" {
   project = var.project_id
-  service = [  "container.googleapis.com",  "cloudresourcemanager.googleapis.com",  "redis.googleapis.com",  "sqladmin.googleapis.com",  "servicenetworking.googleapis.com",  "dns.googleapis.com",]
+  service = "container.googleapis.com,cloudresourcemanager.googleapis.com,redis.googleapis.com,sqladmin.googleapis.com,servicenetworking.googleapis.com,dns.googleapis.com"
+
 
 }
 
