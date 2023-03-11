a70c8d4ab0383e2b853e2df47c9ea8b4238db1a3
rjsharma737 <rjsharma737@gmail.com>
2023-03-11 18:32:40 +0530

    Update apis.tf

--- terraform-network-module/vpc-network/apis.tf
+++ terraform-network-module/vpc-network/apis.tf
@@ -1,6 +1,7 @@
 resource "google_project_service" "enable_apis" {
   project = var.project
-  count   = length(var.apis)
+  #count   = length(var.apis)
+  count = var.enabled ? length(var.apis) : 0
 
   service = var.apis[count.index]
 }
