fc1aa8eead49620321a875cdfe24b81d410e630b
rjsharma737 <rjsharma737@gmail.com>
2023-03-12 15:32:54 +0530

    Update main.tf

--- terraform-network-module/vpn-network/main.tf
+++ terraform-network-module/vpn-network/main.tf
@@ -1,3 +1,4 @@
+/*
 provider "google" {
   project = var.project
   region  = var.region
@@ -61,6 +62,77 @@ resource "google_compute_vpn_tunnel" "tunnel2" {
   #local_traffic_selector = [""]
   #remote_traffic_selector = ["192.168.0.0/19"]
 }
+*/
+  
+
+######
+
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+data "google_compute_network" "network" {
+  name = var.network_name
+}
+
+locals {
+  tunnel1_name    = "${var.project}-ha-vpn-tunnel_1"
+  tunnel2_name    = "${var.project}-ha-vpn-tunnel_2"
+  gateway_name    = "${var.project}-ha-vpn-gateway"
+  intf0_name      = "${var.project}-ha-vpn-gateway-interface0"
+  intf1_name      = "${var.project}-ha-vpn-gateway-interface1"
+  peer_gw_name    = "${var.project}-ha-vpn-peer-gateway"
+  cloud_router    = "${var.project}-ha-vpn-cloud-router"
+  bgp_session1    = "${var.project}-bgp-session1"
+  bgp_session2    = "${var.project}-bgp-session2"
+}
+
+resource "google_compute_vpn_gateway" "gateway" {
+  name        = local.gateway_name
+  network     = data.google_compute_network.network.self_link
+  description = "VPN setup for HQ."
+  labels = {
+    owner = "terraform"
+  }
+
+  depends_on = [
+    data.google_compute_network.network
+  ]
+
+  vpn_interface {
+    name          = local.intf0_name
+    peer_ip       = var.peer_ip1
+    shared_secret = var.shared_secret
+  }
+
+  vpn_interface {
+    name          = local.intf1_name
+    peer_ip       = var.peer_ip2
+    shared_secret = var.shared_secret
+  }
+}
+
+resource "google_compute_vpn_tunnel" "tunnel1" {
+  name                 = local.tunnel1_name
+  peer_ip              = var.peer_ip1
+  shared_secret        = var.shared_secret
+  target_vpn_gateway   = google_compute_vpn_gateway.gateway.self_link
+  shared_secret        = var.shared_secret
+  #local_traffic_selector  = ["0.0.0.0/0"]
+  #remote_traffic_selector = ["192.168.0.0/19"]
+}
+
+resource "google_compute_vpn_tunnel" "tunnel2" {
+  name                 = local.tunnel2_name
+  peer_ip              = var.peer_ip2
+  shared_secret        = var.shared_secret
+  target_vpn_gateway   = google_compute_vpn_gateway.gateway.self_link
+  shared_secret        = var.shared_secret
+  #local_traffic_selector  = ["0.0.0.0/0"]
+  #remote_traffic_selector = ["192.168.0.0/19"]
+}
+
 
 resource "google_compute_router" "router" {
   name    = local.cloud_router
