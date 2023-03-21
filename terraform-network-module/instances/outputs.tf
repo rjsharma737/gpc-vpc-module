bcb8ca863f8a039723e371dba8e2e97c0f682080
rjsharma737 <rjsharma737@gmail.com>
2023-03-22 03:17:23 +0530

    Delete outputs.tf

--- terraform-network-module/instances/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_self_links" {
-  value = google_compute_instance.instance.*.self_link
-}
-
-output "instance_ips" {
-  value = google_compute_instance.instance.*.network_interface.0.network_ip
-}
