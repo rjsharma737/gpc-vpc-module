5ce115da04b87ff655f77c73e7b908770713f971
rjsharma737 <rjsharma737@gmail.com>
2023-03-22 00:22:53 +0530

    Update main.tf

--- terraform-network-module/instances/main.tf
+++ terraform-network-module/instances/main.tf
@@ -1,243 +1,34 @@
-resource "google_compute_instance" "instance" {
-  count = var.instance_count
-  name         = var.instance_names[count.index]
-  machine_type = var.instance_machine_types[count.index]
-  zone         = data.google_compute_subnetwork.subnet.zone
+resource "google_compute_instance" "vm_instances" {
+  count        = var.num_instances
+  name         = var.instance_name[count.index]
+  machine_type = var.machine_type
+  tags         = ["cez-india-hq", "testvm"]
   boot_disk {
     initialize_params {
-      size  = var.instance_boot_disk_sizes[count.index]
-      type  = var.instance_boot_disk_types[count.index]
-      image = var.instance_image
-    }
-  }
-  network_interface {
-    subnetwork = data.google_compute_subnetwork.subnet.self_link
-    access_config {
-      // Ephemeral IP is not requested
+      size = var.boot_disk_size
+      type = var.boot_disk_type
     }
   }
   metadata = {
-    ssh-keys = join("\n", var.instance_ssh_keys)
+    ssh-keys = "${var.ssh_username}:${var.ssh_public_key}"
   }
-  labels = var.instance_labels
-  tags = var.network_tags
-  lifecycle {
-    ignore_changes = [network_interface.0.subnetwork,      labels,      metadata,      tags,    ]
+  network_interface {
+    network = var.vpc_name
+    subnetwork = var.subnet_name
   }
-}
-
-resource "google_compute_disk" "boot_disk" {
-  count = var.instance_count
-  name  = "${google_compute_instance.instance[count.index].name}-boot-disk"
-  type  = var.instance_boot_disk_types[count.index]
-  zone  = data.google_compute_subnetwork.subnet.zone
-  size  = var.instance_boot_disk_sizes[count.index]
-  image = var.instance_image
-  depends_on = [for instance in google_compute_instance.instance : instance.id]
-}
-
-data "google_compute_subnetwork" "subnet" {
-  name   = var.subnet_name
-  region = var.subnet_region
-}
-
-data "google_compute_zones" "zones" {
-  region = var.subnet_region
-}
-
-
-
-
-
-/*
-resource "google_compute_instance" "instance" {
-  count = var.instance_count
-
-  name         = var.instance_names[count.index]
-  machine_type = var.instance_machine_types[count.index]
-  zone         = data.google_compute_subnetwork.subnet.zone
+  labels = {
+    environment = var.environment
+    user-name = var.user_name
+    team-name = var.team_name
+  }
+  boot_disk_auto_delete = true
+  scheduling {
+    automatic_restart = false
+  }
 
   boot_disk {
     initialize_params {
-      size  = var.instance_boot_disk_sizes[count.index]
-      type  = var.instance_boot_disk_types[count.index]
-      image = var.instance_image
-    }
-  }
-
-  network_interface {
-    subnetwork = data.google_compute_subnetwork.subnet.self_link
-
-    access_config {
-      // Ephemeral IP is not requested
-    }
-  }
-
-  metadata = {
-    ssh-keys = join("\n", var.instance_ssh_keys)
-  }
-
-  labels = var.instance_labels
-
-  tags = var.network_tags
-
-  lifecycle {
-    ignore_changes = [network_interface.0.subnetwork,      labels,      metadata,      tags,    ]
-  }
-}
-
-resource "google_compute_disk" "boot_disk" {
-  count = var.instance_count
-
-  name  = "${var.instance_name}-${count.index}"
-  type  = var.boot_disk_type
-  size  = var.boot_disk_size_gb
-
-  zone  = var.zone
-
-  depends_on = [google_compute_instance.instance[count.index].self_link]
-}
-
-
-data "google_compute_subnetwork" "subnet" {
-  name   = var.subnet_name
-  region = var.subnet_region
-}
-
-data "google_compute_zones" "zones" {
-  region = var.subnet_region
-}
-
-*/
-  
-  
-/*
-resource "google_compute_instance" "instance" {
-  count = var.instance_count
-
-  name         = var.instance_names[count.index]
-  machine_type = var.instance_machine_types[count.index]
-  zone         = data.google_compute_subnetwork.subnet.zone
-
-  boot_disk {
-    initialize_params {
-      size  = var.instance_boot_disk_sizes[count.index]
-      type  = var.instance_boot_disk_types[count.index]
-      image = var.instance_image
-    }
-  }
-
-  network_interface {
-    subnetwork = data.google_compute_subnetwork.subnet.self_link
-
-    access_config {
-      // Ephemeral IP is not requested
-    }
-  }
-
-  metadata = {
-    ssh-keys = join("\n", var.instance_ssh_keys)
-  }
-
-  labels = var.instance_labels
-
-  tags = var.network_tags
-
-  lifecycle {
-    ignore_changes = [
-      network_interface[0].subnetwork,
-      labels,
-      metadata,
-      tags,
-    ]
-
-    delete {
-      delete_disks = var.delete_disks_on_instance_delete
+      image = var.image_name
     }
   }
 }
-
-data "google_compute_subnetwork" "subnet" {
-  name          = var.subnet_name
-  region        = var.subnet_region
-  depends_on    = [google_compute_instance.instance] # Wait for instances to be created
-}
-
-data "google_compute_zones" "zones" {
-  region = var.subnet_region
-}
-
-*/
-
-
-/*
-##########################
-
-# Load variables from .tfvars file
-
-# Load variables from .tfvars file
-locals {
-  variables = merge(
-    var,
-    file("${path.module}/variables.tfvars"),
-  )
-}
-
-resource "google_compute_instance" "instance" {
-  count = local.variables.instance_count
-
-  name         = local.variables.instance_names[count.index]
-  machine_type = local.variables.instance_machine_types[count.index]
-  zone         = data.google_compute_subnetwork.subnet.zone
-
-  boot_disk {
-    initialize_params {
-      size  = local.variables.instance_boot_disk_sizes[count.index]
-      type  = local.variables.instance_boot_disk_types[count.index]
-      image = local.variables.instance_image
-    }
-  }
-
-  network_interface {
-    subnetwork = data.google_compute_subnetwork.subnet.self_link
-
-    access_config {
-      # Ephemeral IP is not requested
-    }
-  }
-
-  metadata = {
-    ssh-keys = join("\n", local.variables.instance_ssh_keys)
-  }
-
-  labels = local.variables.instance_labels
-
-  tags = local.variables.network_tags
-
-  lifecycle {
-    ignore_changes = [
-      network_interface[0].subnetwork,
-      labels,
-      metadata,
-      tags,
-    ]
-
-    delete {
-      delete_disks = local.variables.delete_disks_on_instance_delete
-    }
-  }
-}
-
-data "google_compute_subnetwork" "subnet" {
-  name          = local.variables.subnet_name
-  region        = local.variables.subnet_region
-  depends_on    = [google_compute_instance.instance] # Wait for instances to be created
-}
-
-data "google_compute_zones" "zones" {
-  region = local.variables.subnet_region
-}
-
-
-*/
-###########################
