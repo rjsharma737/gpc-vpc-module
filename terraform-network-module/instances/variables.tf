4518846379a648f92c9a3f0eb8cebf73815285d8
rjsharma737 <rjsharma737@gmail.com>
2023-03-22 00:24:00 +0530

    Update variables.tf

--- terraform-network-module/instances/variables.tf
+++ terraform-network-module/instances/variables.tf
@@ -2,65 +2,63 @@ variable "project" {
   description = "The project ID"
 }
 
-variable "instance_count" {
-  type    = number
-  default = 1
+variable "region" {
+  description = "The region where the instances will be created."
 }
 
-variable "vpc_network_name" {
-  description = "Name of the VPC network to launch the instances in."
-  type        = string
+variable "num_instances" {
+  description = "The number of instances to create."
 }
 
+variable "instance_name" {
+  description = "The names of the instances to create."
+  type        = list(string)
+}
+
+variable "machine_type" {
+  description = "The machine type to use for the instances."
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC to create the instances in."
+}
 
 variable "subnet_name" {
-  type    = string
+  description = "The name of the subnet to create the instances in."
 }
 
-variable "subnet_region" {
-  type    = string
+variable "ssh_username" {
+  description = "The SSH username to use for connecting to the instances."
 }
 
-variable "instance_names" {
-  type    = list(string)
+variable "ssh_public_key" {
+  description = "The SSH public key to use for connecting to the instances."
 }
 
-variable "instance_machine_types" {
-  type    = list(string)
-  default = ["e2-medium"]
+variable "boot_disk_size" {
+  description = "The size of the boot disk for the instances."
 }
 
-
-variable "instance_boot_disk_sizes" {
-  type    = list(number)
+variable "boot_disk_type" {
+  description = "The type of the boot disk for the instances. Use 'pd-ssd' for SSD persistent disk and 'pd-standard' for balanced persistent disk."
 }
 
-variable "instance_boot_disk_types" {
-  type    = list(string)
-  default = ["SSD"]
+variable "environment" {
+  description = "The environment label for the instances."
 }
 
-
-variable "instance_ssh_keys" {
-  type    = list(string)
+variable "user_name" {
+  description = "The user name label for the instances."
 }
 
-variable "instance_image" {
-  type    = string
+variable "team_name" {
+  description = "The team name label for the instances."
 }
 
-variable "instance_labels" {
-  type    = map(string)
+variable "image_name" {
+  description = "The name of the image to use for the instances."
 }
 
-variable "delete_disks_on_instance_delete" {
-  type    = bool
-  default = true
-}
-
-variable "network_tags" {
-  type    = list(string)
-}
 
 /*
 variable "gce_ssh_user" {
