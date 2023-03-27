20e1a4f1dbba8a1cb5cae3178f31d57306cf3293
rjsharma737 <rjsharma737@gmail.com>
2023-03-27 23:58:39 +0530

    Update main.tf

--- terraform-network-module/instances/compute-disk/main.tf
+++ terraform-network-module/instances/compute-disk/main.tf
@@ -3,7 +3,7 @@
 ####################
 resource "google_compute_disk" "disk" {
   name                      = var.boot_disk
-  #snapshot                  = var.source_disk_snapshot
+  snapshot                  = var.source_disk_snapshot
   type                      = var.disk_type
   size                      = var.boot_disk_size_gb
   zone                      = var.zone
@@ -20,7 +20,7 @@ resource "google_compute_disk" "addtional_disk" {
   size          = var.disk_size_gb
   zone          = var.zone
   labels        = var.disk_labels
-  #snapshot      = var.source_disk_snapshot_external
+  snapshot      = var.source_disk_snapshot_external
 }
 
 ##############################
@@ -35,5 +35,5 @@ resource "google_compute_region_disk" "addtional_disk" {
   replica_zones = var.replica_zones
   //image = var.source_image
   labels        = var.disk_labels
-  #snapshot      = var.source_disk_snapshot_external
+  snapshot      = var.source_disk_snapshot_external
 }
